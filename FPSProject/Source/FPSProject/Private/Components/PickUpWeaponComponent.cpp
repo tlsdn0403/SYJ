5247b9368e273e3c936e6a2b6b1b83b8595cb092
tlsdn0403@NoteBook <tlsdn0403@gmail.com>
2025-10-08 19:25:53 +0900

    애니메이션 하다 말음

--- FPSProject/Source/FPSProject/Private/Components/PickUpWeaponComponent.cpp
+++ FPSProject/Source/FPSProject/Private/Components/PickUpWeaponComponent.cpp
@@ -2,6 +2,7 @@
 
 #include "Components/PickUpWeaponComponent.h"
 #include "Characters/FPSBaseCharacter.h"
+#include "Weapon/WeaponBase.h"
 #include "Engine/Engine.h" // 디버그 메시지 출력용
 
 UPickUpWeaponComponent::UPickUpWeaponComponent()
@@ -26,7 +27,7 @@ void UPickUpWeaponComponent::OnSphereBeginOverlap(UPrimitiveComponent* Overlappe
 	if (Character && Character->IsPlayerControlled())
 	{
 		// 디버깅 메시지: 플레이어가 무기 장착(픽업) 트리거에 진입했는지 확인
-		UE_LOG(LogTemp, Log, TEXT("[PickUpWeaponComponent] 플레이어 '%s' 가 무기 장착 트리거에 진입했습니다 (소유자: '%s')."),
+		UE_LOG(LogTemp, Log, TEXT("[PickUpWeaponComponent] '%s'  (Owner: '%s')."),
 			*GetNameSafe(Character),
 			*GetNameSafe(GetOwner()));
 
@@ -36,11 +37,12 @@ void UPickUpWeaponComponent::OnSphereBeginOverlap(UPrimitiveComponent* Overlappe
 				-1,
 				2.0f,
 				FColor::Green,
-				FString::Printf(TEXT("무기 장착: %s"), *GetNameSafe(Character))
+				FString::Printf(TEXT("Attatch Weapon: %s"), *GetNameSafe(Character))
 			);
 		}
 
 		OnPickUp.Broadcast(Character);
+		Character->SetCurrentWeapon(Cast<AWeaponBase>(GetOwner()));
 		OnComponentBeginOverlap.RemoveAll(this); // 한 번만 실행
 	}
 }

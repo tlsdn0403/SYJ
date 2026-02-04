517d77e5405dac254975e9501d0b12c6ce0c1fb8
tlsdn0403@NoteBook <tlsdn0403@gmail.com>
2026-02-04 19:38:25 +0900

    좀비가 캐릭터 쳐다보게

---
+++ FPSProject/Source/FPSProject/Public/Zombie/AIZombieController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "AIZombieController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSPROJECT_API AAIZombieController : public AAIController
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void BeginPlay() override;
+};

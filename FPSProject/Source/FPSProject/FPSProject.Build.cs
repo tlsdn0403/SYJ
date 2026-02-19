cbad6c97a4ef06547340312657a2e2f3431993aa
tlsdn0403@NoteBook <tlsdn0403@gmail.com>
2026-02-19 18:24:42 +0900

    2.19

--- FPSProject/Source/FPSProject/FPSProject.Build.cs
+++ FPSProject/Source/FPSProject/FPSProject.Build.cs
@@ -8,7 +8,7 @@ public class FPSProject : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AIModule","GameplayTasks" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 

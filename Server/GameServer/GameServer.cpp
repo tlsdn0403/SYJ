3bef1a7cd72695a856a03a5b93fa6b57f743537f
unknown <0412juhwan@gmail.com>
2026-02-20 21:19:35 +0900

    session

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -12,7 +12,19 @@
 
 class GameSession : public Session
 {
+public:
+	virtual int32 OnRecv(BYTE* buffer, int32 len) override
+	{
+		// Echo
+		cout << "OnRecv Len = " << len << endl;
+		Send(buffer, len);
+		return len;
+	}
 
+	virtual void OnSend(int32 len) override
+	{
+		cout << "OnSend Len = " << len << endl;
+	}
 };
 
 int main()
@@ -20,7 +32,7 @@ int main()
 	ServerServiceRef service = MakeShared<ServerService>(
 		NetAddress(L"127.0.0.1", 7777), 
 		MakeShared<IocpCore>(),
-		MakeShared<Session>,	// TODO : SessionManager 등
+		MakeShared<GameSession>,	// TODO : SessionManager 등
 		100);
 
 	ASSERT_CRASH(service->Start());

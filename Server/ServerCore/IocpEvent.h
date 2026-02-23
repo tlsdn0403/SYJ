7589c48367d0a0fc4deebb2eaf659c81a8a200a8
unknown <0412juhwan@gmail.com>
2026-02-23 15:58:14 +0900

    sendBuffer

--- Server/ServerCore/IocpEvent.h
+++ Server/ServerCore/IocpEvent.h
@@ -80,6 +80,5 @@ class SendEvent : public IocpEvent
 public:
 	SendEvent() : IocpEvent(EventType::Send) { }
 
-	// TEMP
-	vector<BYTE> buffer;
+	Vector<SendBufferRef> sendBuffers;
 };
--- Server/ServerCore/Session.h
+++ Server/ServerCore/Session.h
@@ -26,8 +26,8 @@ public:
 	virtual ~Session();
 
 public:
-	/* 외부에서 사용 */
-	void				Send(BYTE* buffer, int32 len);
+						/* 외부에서 사용 */
+	void				Send(SendBufferRef sendBuffer);
 	bool				Connect();
 	void				Disconnect(const WCHAR* cause);
 
@@ -35,7 +35,7 @@ public:
 	void				SetService(shared_ptr<Service> service) { _service = service; }
 
 public:
-	/* 정보 관련 */
+						/* 정보 관련 */
 	void				SetNetAddress(NetAddress address) { _netAddress = address; }
 	NetAddress			GetAddress() { return _netAddress; }
 	SOCKET				GetSocket() { return _socket; }
@@ -43,26 +43,26 @@ public:
 	SessionRef			GetSessionRef() { return static_pointer_cast<Session>(shared_from_this()); }
 
 private:
-	/* 인터페이스 구현 */
+						/* 인터페이스 구현 */
 	virtual HANDLE		GetHandle() override;
 	virtual void		Dispatch(class IocpEvent* iocpEvent, int32 numOfBytes = 0) override;
 
 private:
-	/* 전송 관련 */
+						/* 전송 관련 */
 	bool				RegisterConnect();
 	bool				RegisterDisconnect();
 	void				RegisterRecv();
-	void				RegisterSend(SendEvent* sendEvent);
+	void				RegisterSend();
 
 	void				ProcessConnect();
 	void				ProcessDisconnect();
 	void				ProcessRecv(int32 numOfBytes);
-	void				ProcessSend(SendEvent* sendEvent, int32 numOfBytes);
+	void				ProcessSend(int32 numOfBytes);
 
 	void				HandleError(int32 errorCode);
 
 protected:
-	/* 컨텐츠 코드에서 재정의 */
+						/* 컨텐츠 코드에서 재정의 */
 	virtual void		OnConnected() {}
 	virtual int32		OnRecv(BYTE* buffer, int32 len) { return len; }
 	virtual void		OnSend(int32 len) {}
@@ -77,14 +77,17 @@ private:
 private:
 	USE_LOCK;
 
-	/* 수신 관련 */
-	RecvBuffer			_recvBuffer;
+							/* 수신 관련 */
+	RecvBuffer				_recvBuffer;
 
-	/* 송신 관련 */
+							/* 송신 관련 */
+	Queue<SendBufferRef>	_sendQueue;
+	Atomic<bool>			_sendRegistered = false;
 
 private:
-	/* IocpEvent 재사용 */
+						/* IocpEvent 재사용 */
 	ConnectEvent		_connectEvent;
 	DisconnectEvent		_disconnectEvent;
 	RecvEvent			_recvEvent;
+	SendEvent			_sendEvent;
 };

57edd3ae0dc00d73985f007a79e1221377dec6c5
unknown <0412juhwan@gmail.com>
2026-02-24 23:49:26 +0900

    buffer helper

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -4,6 +4,7 @@
 #include "Session.h"
 #include "GameSession.h"
 #include "GameSessionManager.h"
+#include "BufferWriter.h"
 
 int main()
 {
@@ -32,12 +33,18 @@ int main()
 	{
 		SendBufferRef sendBuffer = GSendBufferManager->Open(4096);
 
-		BYTE* buffer = sendBuffer->Buffer();
+		BufferWriter bw(sendBuffer->Buffer(), 4096);
 
-		((PacketHeader*)buffer)->size = (sizeof(sendData) + sizeof(PacketHeader));
-		((PacketHeader*)buffer)->id = 1;	// 1 : Hello Msg
-		::memcpy(&buffer[4], sendData, sizeof(sendData));
-		sendBuffer->Close(sizeof(sendData) + sizeof(PacketHeader));
+		PacketHeader* header = bw.Reserve<PacketHeader>();
+
+		// id(uint64), 체력(uint32), 공격력(uint16)
+		bw << (uint64)1001 << (uint32)100 << (uint16)10;
+		bw.Write(sendData, sizeof(sendData));
+		
+		header->size = bw.WriteSize();
+		header->id = 1;	// 1 : Test Msg
+
+		sendBuffer->Close(bw.WriteSize());
 
 		GSessionManager.Broadcast(sendBuffer);
 

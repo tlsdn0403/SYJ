0fc3ce03cedb1c73f3f2398d787558f47e9f9030
yunjinan@MYPC <anyoonjin0@gmail.com>
2026-02-24 23:32:33 +0900

    다른 파일에서 timer 파일 가져온 후 수정 경로 추가

---
+++ FPSProject/Source/FPSProject/Public/HUD/BaseUI.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "HUD/BaseUI.h"
+#include "Components/CanvasPanel.h"
+#include "Components/TextBlock.h"
+
+/*
+NativeOnInitialized : 위젯이 생성될 때 딱 한 번 호출,  에디터 편집 시에도 생성될 때 호출
+NativeConstruct : AddToViewport 시 호출, Viewport 에 Add 될 때마다 호출됨
+NativeDestruct : RemoveFromParent(RemoveFromViewport) 시 호출, Viewport 에서 제거될 때마다 호출됨
+*/
+
+void UBaseUI::NativeConstruct()
+{
+	Super::NativeConstruct(); // 부모 클래스의 NativeConstruct 호출
+
+	if (TimerText)
+	{
+		TimerText->SetText(FText::FromString(TEXT("05:00")));
+		/*
+			SetText()는 FText만 받기 때문에 
+		*/
+		GetWorld()->GetTimerManager().SetTimer(TimerHandle,this,&UBaseUI::UpdateTimer,1.0f,true);
+	}
+}
+
+
+void UBaseUI:: UpdateTimer()
+{
+	totalTime -= 1;
+	if(totalTime < 0)
+	{
+		GetWorld()->GetTimerManager().ClearTimer(TimerHandle);
+		return;
+	}
+	if (totalTime <= 5) {
+		PlayAni_PopUp();
+	}
+	else if (totalTime <= 20)	//20초 이하일 때 진동 애니메이션 실행
+	{
+		PlayAni_Vibration();
+	}
+
+	FString Stime = FString::Printf(TEXT("%02d:%02d"), totalTime/60,totalTime%60);
+	TimerText->SetText(FText::FromString(Stime));
+}
---
+++ FPSProject/Source/FPSProject/Public/HUD/BaseUI.h
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "BaseUI.generated.h"
+
+/**
+ * 
+ */
+class UTextBlock;	// 전방 선언
+class UCanvasPanel;
+
+UCLASS()
+class FPSPROJECT_API UBaseUI : public UUserWidget
+{
+	GENERATED_BODY()
+
+	/*
+	meta= (BindWidget) :UMG위젯 자동 바인딩, C++ 멤버 변수와 UMG 위젯을 연결. 
+	*/
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget, AllowPrivateAccess = "true"))
+	TObjectPtr<UCanvasPanel> RootCanvas;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget, AllowPrivateAccess = "true"))
+	//UTextBlock* TimerText; << 이건 옛날방법
+	TObjectPtr<UTextBlock>TimerText; //UE5에선 이 방식 사용
+
+protected:
+	virtual void NativeConstruct() override;
+
+	UPROPERTY(meta=(BindWidgetAnim, AllowPrivateAccess), Transient)
+	/*
+	Transient < 이 속성은 객체가 직렬화되지 않도록 지정, 즉 저장이나 로드 시에 이 변수의 값이 포함되지 않음. => 애니 데이터 저장x라 로딩시 항상 새로 초기화 됨.
+	*/
+	UWidgetAnimation* TCountVibration;	//변수 이름은 애니 이름과 동일해야함.
+
+	UPROPERTY(meta = (BindWidgetAnim, AllowPrivateAccess), Transient)
+	UWidgetAnimation* TPopUp;	
+
+public:
+
+	int32 totalTime = 30; // 5 minutes in seconds
+	FTimerHandle TimerHandle; 
+	void UpdateTimer();
+	void PlayAni_Vibration() {PlayAnimation(TCountVibration);}
+	void PlayAni_PopUp() { PlayAnimation(TPopUp); }
+};

907c0c2fe7188bba9f3f4a653499e6588ff1c7ba
unknown <0412juhwan@gmail.com>
2026-02-26 16:46:12 +0900

    packet handler

--- Server/ServerCore/BufferWriter.h
+++ Server/ServerCore/BufferWriter.h
@@ -24,9 +24,6 @@ public:
 	T*			Reserve();
 
 	template<typename T>
-	BufferWriter& operator<<(const T& src);
-
-	template<typename T>
 	BufferWriter& operator<<(T&& src);
 
 private:
@@ -46,18 +43,12 @@ T* BufferWriter::Reserve()
 	return ret;
 }
 
-template<typename T>
-BufferWriter& BufferWriter::operator<<(const T& src)
-{
-	*reinterpret_cast<T*>(&_buffer[_pos]) = src;
-	_pos += sizeof(T);
-	return *this;
-}
 
 template<typename T>
 BufferWriter& BufferWriter::operator<<(T&& src)
 {
-	*reinterpret_cast<T*>(&_buffer[_pos]) = std::move(src);
+	using DataType = std::remove_reference_t<T>;
+	*reinterpret_cast<DataType*>(&_buffer[_pos]) = std::forward<DataType>(src);
 	_pos += sizeof(T);
 	return *this;
 }

11ac92673bc9ea973c792afd471c5d2ed4a1c3ee
unknown <0412juhwan@gmail.com>
2026-02-27 00:39:00 +0900

    패킷 직렬화

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -6,6 +6,7 @@
 #include "GameSessionManager.h"
 #include "BufferWriter.h"
 #include "ServerPacketHandler.h"
+#include <tchar.h>
 
 int main()
 {
@@ -28,12 +29,37 @@ int main()
 			});
 	}	
 
-	char sendData[1000] = "Hello World";
+	WCHAR sendData3[1000] = L"가";	// UTF16
 
 	while (true)
 	{
-		vector<BuffData> buffs{ BuffData{100, 1.5f}, BuffData{200, 2.3f}, BuffData{300, 0.7f} };
-		SendBufferRef sendBuffer = ServerPacketHandler::Make_S_TEST(1001, 100, 10, buffs);	
+		PKT_S_TEST_WRITE pktWriter(1001, 100, 10);
+
+		PKT_S_TEST_WRITE::BuffsList buffList = pktWriter.ReserveBuffList(3);
+		buffList[0] = { 100, 1.5f };
+		buffList[1] = { 200, 2.3f };
+		buffList[2] = { 300, 0.7f };
+
+		PKT_S_TEST_WRITE::BuffsVictimsList vic0 = pktWriter.ReserveBuffsVictimsList(&buffList[0], 3);
+		{
+			vic0[0] = 1000;
+			vic0[1] = 2000;
+			vic0[2] = 3000;
+		}
+
+		PKT_S_TEST_WRITE::BuffsVictimsList vic1 = pktWriter.ReserveBuffsVictimsList(&buffList[1], 1);
+		{
+			vic1[0] = 1000;
+		}
+
+		PKT_S_TEST_WRITE::BuffsVictimsList vic2 = pktWriter.ReserveBuffsVictimsList(&buffList[2], 2);
+		{
+			vic2[0] = 3000;
+			vic2[1] = 5000;
+		}
+
+		SendBufferRef sendBuffer = pktWriter.CloseAndReturn();
+
 		GSessionManager.Broadcast(sendBuffer);
 
 		this_thread::sleep_for(250ms);
--- Server/GameServer/ServerPacketHandler.cpp
+++ Server/GameServer/ServerPacketHandler.cpp
@@ -16,29 +16,3 @@ void ServerPacketHandler::HandlePacket(BYTE* buffer, int32 len)
 		break;
 	}
 }
-
-SendBufferRef ServerPacketHandler::Make_S_TEST(uint64 id, uint32 hp, uint16 attack, vector<BuffData> buffs)
-{
-	SendBufferRef sendBuffer = GSendBufferManager->Open(4096);
-
-	BufferWriter bw(sendBuffer->Buffer(), sendBuffer->AllocSize());
-
-	PacketHeader* header = bw.Reserve<PacketHeader>();
-
-	// id(uint64), 체력(uint32), 공격력(uint16)
-	bw << id << hp << attack;
-
-	// 가변 데이터
-	bw << (uint16)buffs.size();
-	for (BuffData& buff : buffs)
-	{
-		bw << buff.buffId << buff.remainTime;
-	}
-
-	header->size = bw.WriteSize();
-	header->id = S_TEST;	// 1 : Test Msg
-
-	sendBuffer->Close(bw.WriteSize());
-
-	return sendBuffer;
-}

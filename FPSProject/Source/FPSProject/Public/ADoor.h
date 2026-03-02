fb1bf89a26b12a4c93907b24a14e6d2f851dbaee
yunjinan@MYPC <anyoonjin0@gmail.com>
2026-03-02 16:53:59 +0900

    상호작용 문bp 제작. 후에 범위설정 박스로 변경하기

---
+++ FPSProject/Source/FPSProject/Public/ADoor.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interface/InteractInterface.h"
+#include "ADoor.generated.h"
+
+
+//상호작용이 있을때만 활동할 것이라 그 외 이벤트가 필요없
+
+class UInteractTriggerComponent;
+
+UCLASS()
+class FPSPROJECT_API AADoor : public AActor, public IInteractInterface
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AADoor();
+
+protected:
+	// Called when the game starts or when spawned
+	//virtual void BeginPlay() override;
+
+
+
+public:	
+	// Called every frame
+	//virtual void Tick(float DeltaTime) override;
+	USceneComponent* SceneRoot;	//여러 컴포넌트를 묶어주는 역할, 위젯도 추가될 것이기에 추가.
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UStaticMeshComponent* StaticMeshComp;
+
+	// 상호작용 범위 컴포넌트
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Interaction")
+	UInteractTriggerComponent* InteractTrigger;
+
+	// 인터페이스 함수 오버라이드 (F키 눌렀을 때 실행될 내용)
+	virtual void Interact_Implementation(AFPSBaseCharacter* Character) override;
+};

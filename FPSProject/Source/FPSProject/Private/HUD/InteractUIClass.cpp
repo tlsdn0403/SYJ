b1e1a21e5f1a1efdea258d7f7f97cdfb6646f1c9
yunjinan@MYPC <anyoonjin0@gmail.com>
2026-03-05 23:20:27 +0900

    원상태복구 및 문열기닫기 텍스트 영어로 설정완

--- FPSProject/Source/FPSProject/Private/HUD/InteractUIClass.cpp
+++ FPSProject/Source/FPSProject/Private/HUD/InteractUIClass.cpp
@@ -9,6 +9,7 @@ void UInteractUIClass::NativeConstruct()
 	Super::NativeConstruct();
 	// 애니메이션이랑 텍스트 블록이 제대로 바인딩 되었는지 확인
 	InteractText->SetIsEnabled(false);	//상호작용 안되게
+	InteractText->SetText(FText::FromString(TEXT("Open")));
 }
 void UInteractUIClass::PlayAni_PopUp(bool bOpen)
 {
@@ -16,7 +17,6 @@ void UInteractUIClass::PlayAni_PopUp(bool bOpen)
 	PlayAnimation(PopUp);
 
 	//애니메이션의 마지막 프레임이 띄워져있기때문에 알아서 유지가 됨.
-	InteractText->SetText(FText::FromString(bOpen ? TEXT("문 열기") : TEXT("close")));
 }
 
 void UInteractUIClass::RePlayAni_PopUp() // 범위 벗어나서 사라짐.
@@ -26,11 +26,8 @@ void UInteractUIClass::RePlayAni_PopUp() // 범위 벗어나서 사라짐.
 		PlayAnimation(PopUp, 0.f, 1, EUMGSequencePlayMode::Reverse); //애니메이션 닫기
 	}
 }
-void UInteractUIClass:: SetDoorOpenState(bool bIsOpen)
+
+void UInteractUIClass::SetDoorInteractText(const bool bOpen)
 {
-	if (InteractText)
-	{
-		//InteractText->SetText(FText::);
-		InteractText->SetText(FText::FromString(TEXT("open")));
-	}
+	InteractText->SetText(FText::FromString(bOpen ? TEXT("Close") : TEXT("Open")));
 }

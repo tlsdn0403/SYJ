cb8b12dbe8c8fdb9a1520adee92e76b88a3ec1ba
yunjinan@MYPC <anyoonjin0@gmail.com>
2026-06-02 17:34:36 +0900

    L2 UI class추가

---
+++ FPSProject/Source/FPSProject/Private/HUD/L2BaseUI.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HUD/L2BaseUI.h"
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+
+
+void UL2BaseUI::NativeConstruct()
+{
+	Super::NativeConstruct();
+}
+
